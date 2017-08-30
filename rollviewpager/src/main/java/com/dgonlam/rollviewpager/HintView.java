e3ee851d546fe955ba5b6144ea4ca1291e606dc3
DgonLam <531075100@qq.com>
2017-08-30 11:22:36 +0800

    init
    init

---
+++ rollviewpager/src/main/java/com/dgonlam/rollviewpager/HintView.java
@@ -0,0 +1,10 @@
+package com.dgonlam.rollviewpager;
+
+
+public interface HintView {
+
+	void initView(int length, int gravity);
+
+	void setCurrent(int current);
+}
+
---
+++ rollviewpager/src/main/java/com/dgonlam/rollviewpager/OnItemClickListener.java
@@ -0,0 +1,6 @@
+package com.dgonlam.rollviewpager;
+
+
+public interface OnItemClickListener {
+    void onItemClick(int position);
+}
